2277ad376ad483ad08af4c9698e7f04477f3fcc7
Joshua Doud <bigjj3393@yahoo.com>
2022-10-13 00:41:04 -0700

    commit tree f69900473777cae3e8dde758aaff3ea71426e6ab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42)
+}
+
+function distanceFromHqInFeet(location) {
+    distanceFromHqInBlocks(location);
+    return (distanceFromHqInBlocks(location) * 264)
+}
+
+function distanceTravelledInFeet(a,b) {
+    return Math.abs(a-b) * 264
+}
+
+function calculatesFarePrice(a,b) {
+    distanceTravelledInFeet(a,b)
+    if (distanceTravelledInFeet(a,b) < 400)
+        return 0 
+    else if (distanceTravelledInFeet(a,b) <= 2000)
+        return (distanceTravelledInFeet(a,b) - 400) * .02
+    else if (distanceTravelledInFeet(a,b) <=2500)
+        return 25
+    else if (distanceTravelledInFeet(a,b) >2500)
+        return `cannot travel that far`
+}
